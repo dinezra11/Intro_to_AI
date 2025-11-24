e33bd6637a3c9cf62954271ab372cf73b60fa6fc
dinezra <dinezra11@gmail.com>
2025-11-24 21:05:06 +0200

    Increase simulation steps

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from environments.environment import Environment
 
 env = Environment(yaml_path='environments/environment_config.yaml')
-for i in range(10):
+for i in range(100):
     env.log_environment()
     env.step()
