724afef63af6c25616c5b1851794bc2a0c5576a5
dinezra <dinezra11@gmail.com>
2025-12-30 18:55:14 +0200

    a bit larger environment

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from environments.environment import Environment
 import os
 os.system('')
 
-env = Environment(yaml_path='environments/environment_minimax_config.yaml')
-for i in range(20):
+env = Environment(yaml_path='environments/environment_minimax_large_config.yaml')
+for i in range(100):
     env.log_environment()
     env.step()
